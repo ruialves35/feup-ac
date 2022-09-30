76ad1454d0f224313e69b8a281d2e8ec715205b9
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-09-30 12:43:52 +0100

    Clean loan_dev

--- src/clean.py
+++ src/clean.py
@@ -1,5 +1,7 @@
+import shutil
 from cleaning.account import clean_account
 from cleaning.card_dev import clean_card_dev
+from cleaning.loan_dev import clean_loan_dev
 
 RAW_PATH = "../assets/raw/"
 CLEAN_PATH = "../assets/clean/"
@@ -8,5 +10,15 @@ print("Cleaning data files inside assets/raw/ ...\n")
 
 print("Cleaning account.csv ...")
 clean_account(RAW_PATH + "account.csv", CLEAN_PATH + "account.csv")
+
 print("Cleaning card_dev.csv ...")
 clean_card_dev(RAW_PATH + "card_dev.csv", CLEAN_PATH + "card_dev.csv")
+
+print("Copying disp.csv (no cleaning) ...")
+shutil.copy(RAW_PATH + "disp.csv", CLEAN_PATH + "disp.csv")
+
+print("Copying district.csv (no cleaning) ...")
+shutil.copy(RAW_PATH + "district.csv", CLEAN_PATH + "district.csv")
+
+print("Cleaning loan_dev.csv ...")
+clean_loan_dev(RAW_PATH + "loan_dev.csv", CLEAN_PATH + "loan_dev.csv")
---
+++ src/cleaning/loan_dev.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def clean_loan_dev(rawPath, cleanPath):
+    df = pd.read_csv(rawPath, sep=";", dtype= {
+        "loan_id": "int64",
+        "account_id": "int64",
+        "date": "object",
+        "amount": "int64",
+        "duration": "int64",
+        "payments": "int64",
+        "status": "category",
+    })
+    df['date'] = pd.to_datetime(df['date'], format="%y%m%d")
+    df['status'] = df['status'].cat.add_categories([0])
+    df.loc[df.status == "-1", "status"] = 0
+    df = df.rename(columns={"status": "paid"})
+    df.to_csv(cleanPath, index=False)
