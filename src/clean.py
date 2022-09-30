f998ba26666688d2dda93e4d9ad8de1c2b5d9f60
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-09-30 12:16:56 +0100

    Clean card_dev

--- src/clean.py
+++ src/clean.py
@@ -1,9 +1,12 @@
 from cleaning.account import clean_account
+from cleaning.card_dev import clean_card_dev
 
-RAW_PATH = "../../assets/raw/"
-CLEAN_PATH = "../../assets/clean/"
+RAW_PATH = "../assets/raw/"
+CLEAN_PATH = "../assets/clean/"
 
 print("Cleaning data files inside assets/raw/ ...\n")
 
 print("Cleaning account.csv ...")
 clean_account(RAW_PATH + "account.csv", CLEAN_PATH + "account.csv")
+print("Cleaning card_dev.csv ...")
+clean_card_dev(RAW_PATH + "card_dev.csv", CLEAN_PATH + "card_dev.csv")
