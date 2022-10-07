005974208e90f0ffa129f0caa7d798c0149f6c00
up201905853 <up201905853@fe.up.pt>
2022-10-07 11:58:23 +0100

    Checking for duplicates in client.csv

--- src/cleaning/client.py
+++ src/cleaning/client.py
@@ -7,6 +7,7 @@ def clear_month(date):
   else:
     return "19" + date[0:2] + "-" + str(month).zfill(2) + "-" + date[4:]
 
+
 def clean_client(rawPath, cleanPath):
   df = pd.read_csv(rawPath, sep=";", dtype= {
     "client_id": "int64",
@@ -14,9 +15,13 @@ def clean_client(rawPath, cleanPath):
     "district_id": "int64",
   })
 
+  # format birth_number and add gender column
   df["birth_number"] = df["birth_number"].astype(str)
   df["gender"] = df["birth_number"].apply(lambda date: 0 if int(date[2:4]) > 50 else 1)
   df["birth_number"] = df["birth_number"].apply(clear_month)
+
+  # return number of duplicates from df["client_id"] column
+  print("Number of duplicates in client_id column: ", df["client_id"].duplicated().sum())
 
   df.to_csv(cleanPath, sep=";", index=False)  
 
