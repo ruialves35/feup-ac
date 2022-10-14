459f13b9045f392624a249221191d68b63c8a0b5
João Mesquita <monkinsmurf@gmail.com>
2022-10-14 15:00:58 +0100

    Add: District cleaning -> Rename columns and replacing '?' by empty string

--- src/cleaning/district.py
+++ src/cleaning/district.py
@@ -12,11 +12,7 @@ def clean_district(rawPath, cleanPath):
         "no. of municipalities with inhabitants >10000 ": "int64",
         "ratio of urban inhabitants ": "float64",
         "average salary ": "int64",
-        "unemploymant rate '95 ": "float64",
-        "unemploymant rate '96 ": "float64",
         "no. of enterpreneurs per 1000 inhabitants ": "float64",
-        "no. of commited crimes '95 ": "int64",
-        "no. of commited crimes '96 ": "int64",
     })
 
     df.rename(columns=lambda x: x.strip(), inplace=True)
@@ -26,13 +22,20 @@ def clean_district(rawPath, cleanPath):
         "no. of municipalities with inhabitants 500-1999": "municip500_1999",
         "no. of municipalities with inhabitants 2000-9999": "municip2000_9999",
         "no. of municipalities with inhabitants >10000": "municip10000",
-        "ratio of urban inhabitants ": "urban_ratio",
-        "average salary ": "avg_salary",
-        "unemploymant rate '95 ": "unemp_rate95",
-        "unemploymant rate '96 ": "unemp_rate96",
-        "no. of enterpreneurs per 1000 inhabitants ": "num_entrepreneurs",
-        "no. of commited crimes '95 ": "num_crimes95",
-        "no. of commited crimes '96 ": "num_crimes96",
+        "ratio of urban inhabitants": "urban_ratio",
+        "average salary": "avg_salary",
+        "unemploymant rate '95": "unemp_rate95",
+        "unemploymant rate '96": "unemp_rate96",
+        "no. of enterpreneurs per 1000 inhabitants": "num_entrepreneurs",
+        "no. of commited crimes '95": "num_crimes95",
+        "no. of commited crimes '96": "num_crimes96",
     }, inplace=True)
+
+    # Replace "?" occurrences by an empty string
+    df["unemp_rate95"] = df["unemp_rate95"].replace("?", "")
+    df["unemp_rate96"] = df["unemp_rate96"].replace("?", "")
+    df["num_crimes95"] = df["num_crimes95"].replace("?", "")
+    df["num_crimes96"] = df["num_crimes96"].replace("?", "")
+
 
     df.to_csv(cleanPath, sep=",", index=False)
