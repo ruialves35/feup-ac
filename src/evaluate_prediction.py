053a8a261cf26a861811dbec622b5a78441c3682
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-11-27 16:54:03 +0000

    Added false rates

--- src/evaluate_prediction.py
+++ src/evaluate_prediction.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 
-SHOW_GRAPH = True
+SHOW_GRAPH = False
 
 SUBMISSION_PATH = "../assets/results/submission.csv"
 RESULTS_PATH = "../assets/complete_clean/loan_dev.csv"
@@ -13,6 +13,30 @@ mix_pd = pd.merge(submission_pd, results_pd, left_on='Id', right_on='loan_id', h
 
 submission_scores = mix_pd["Predicted"].values.tolist()
 results = mix_pd["paid"].values.tolist()
+
+# calculate confusion matrix
+tp = 0
+tn = 0
+fp = 0
+fn = 0
+for i in range(len(results)):
+    if results[i] == 1:
+        if submission_scores[i] < 0.5:
+            tn += 1
+        else:
+            fp += 1
+    else:
+        if submission_scores[i] < 0.5:
+            fn += 1
+        else:
+            tp += 1
+
+print("True Positive: ", tp)
+print("True Negative: ", tn)
+print("False Positive: ", fp)
+print("False Negative: ", fn)
+print("False negative rate", (fn / (fn + tn)) * 100, "%")
+print("False positive rate", (fp / (fp + tp)) * 100, "%")
 
 fpr, tpr, thresholds = roc_curve(results, submission_scores, pos_label=0)
 auc = auc(fpr, tpr)
